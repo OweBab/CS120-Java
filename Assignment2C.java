6e7b1af5a0dcc6fed6601d2203edcf612bf207d0
OweBab <owebab78382@nbss.nbed.nb.ca>
2026-09-09 09:45:00 -0300

    Add files via upload

---
+++ Assignment2C.java
@@ -0,0 +1,14 @@
+class Assignment2C
+{
+    public static void main(String[] args)
+    {
+        double num = 127.5192683;
+
+        System.out.printf("Number = %.7f\n", num);
+        System.out.printf("%-16s%.1f\n", "One decimal", num);
+        System.out.printf("%-16s%.2f\n", "Two decimals", num);
+        System.out.printf("%-16s%.3f\n", "Three decimals", num);
+        System.out.printf("%-16s%.4f\n", "Four decimals", num);
+        System.out.printf("%-16s%.5f\n", "Five decimals", num);
+    }
+}
